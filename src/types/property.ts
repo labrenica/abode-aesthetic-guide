8e677d4c2cb64c8a185a3cc188d6286922826e66
Louix A. <louix.abrenica@gmail.com>
2025-09-07 16:53:40 -0700

    changed extra location props

--- src/types/property.ts
+++ src/types/property.ts
@@ -8,16 +8,11 @@ export interface Property {
   slug?: string;
 
   // Break down location
-  location: {
-    address: string;
-    city: string;
-    state: string;
-    zip: string;
-  };
   address: string;
   city: string;
   state: string;
   zip: string;
+  
 
   price: number;
   bedrooms: number;
